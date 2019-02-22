aa66bccd407d07d2789a5017993f8bca24bac0c3
Hannah <hannah.bohlen.2021@student.lhschools.org>
2019-02-22 07:42:58 -0500

    Created Second Method- Multiply

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,14 +4,29 @@ public class Main {
 
 
     public static void main(String[] args) {
-    Greeting("Bob");
-    Greeting("Sarah");
-    Greeting("Dan");
-    Greeting("Jessica");
+        Greeting("Bob.");
+        Greeting("Sarah.");
+        Greeting("Dan.");
+        Greeting("Jessica.");
+
+        System.out.println(" ");
+
+        int a = 12;
+        int b = 43;
+        System.out.println(multiply(a, b));
+        int c = multiply(a, b);
+        int d = multiply(c, b);
+
 
     }
 
     public static void Greeting(String names) {
-        System.out.println("Hello, " + names);
+        System.out.println("Good morning, " + names);
+    }
+
+    public static int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
     }
 }
+
+
