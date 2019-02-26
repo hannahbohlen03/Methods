cc2eda56e72555108e55d0f2987ac109078b1639
Hannah <hannah.bohlen.2021@student.lhschools.org>
2019-02-26 07:52:02 -0500

    Fixed Some Formatting

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -27,43 +27,48 @@ public class Main {
 
         int a = 12;
         int b = 43;
-        System.out.println("This number is " + a + " times " + b + ".");
+        System.out.println(a + " times " + b + " is ");
         System.out.println(multiply(a, b));
 
         System.out.println(" ");
 
         int x = 435;
         int y = 24;
-        System.out.println(x + " divided by " + y + " is the number below.");
-        System.out.println(Divide(x,y));
+        System.out.println(x + " divided by " + y + " is ");
+        System.out.println(Divide(x, y));
 
         System.out.println(" ");
 
-        System.out.println("Since " + y + " does not fit into " + x + " evenly, the modulus of " + x + " by " + y + " is the number below.");
-        System.out.println(Modulus(x,y));
+        System.out.println("Since " + y + " does not fit into " + x + " evenly, the modulus of " + x + " by " + y + " is ");
+        System.out.println(Modulus(x, y));
 
     }
 
     public static void Greeting(String names) {
         System.out.println("Good morning, " + names);
     }
+
     public static int multiply(int firstNumber, int secondNumber) {
         return firstNumber * secondNumber;
     }
-    public static void Detail(String adjectives){
-        System.out.println("The lion was very " + adjectives +".");
+
+    public static void Detail(String adjectives) {
+        System.out.println("The lion was very " + adjectives + ".");
     }
-    public static void Preference(String animal){
-        System.out.println("My favorite animal is " + animal + ".");
+
+    public static void Preference(String animal) {
+        System.out.println("My favorite animal is a " + animal + ".");
     }
-    public static int Divide(int firstNumber, int secondNumber){
+
+    public static int Divide(int firstNumber, int secondNumber) {
         return firstNumber / secondNumber;
     }
-    public static int Modulus(int firstNumber, int secondNumber){
+
+    public static int Modulus(int firstNumber, int secondNumber) {
         return firstNumber % secondNumber;
     }
 
-    }
+}
 
 
 
